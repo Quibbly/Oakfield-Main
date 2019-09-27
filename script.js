435f29589453fc706f87315763c045a794a6ecee
Quibbly <owenhedwards@gmail.com>
2019-09-28 00:53:36 +0100

    Smooth scrolling, scroll button functionality, media queries for larger screens added.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function scrollA() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 906; // For Chrome, Firefox, IE and Opera
+} 
+
+
+function scrollB() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop =2680; // For Chrome, Firefox, IE and Opera
+} 
+
+function scrollC() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 2006; // For Chrome, Firefox, IE and Opera
+} 
