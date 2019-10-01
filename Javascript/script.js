08e37d592dba4093bb0e7b63d66e11fee1386203
Quibbly <owenhedwards@gmail.com>
2019-10-01 11:01:44 +0100

    Changed top arrow and button height, Adjusted arrow JS move amount

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,4 @@
-
-
 function scrollA() {
    document.body.scrollTop = 0; // For Safari
-   document.documentElement.scrollTop = 990; // For Chrome, Firefox, IE and Opera
+   document.documentElement.scrollTop = 800; // For Chrome, Firefox, IE and Opera
 } 
