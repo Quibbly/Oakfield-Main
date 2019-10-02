978414198cef00ceeb2716ad1fb10080d2704555
Quibbly <owenhedwards@gmail.com>
2019-10-02 19:41:51 +0100

    Finalised Portfolio JS filter (now also uses lightbox)

--- Javascript/portfolio.js
+++ Javascript/portfolio.js
@@ -1,38 +1,53 @@
 let portfolioGallery = document.getElementById('port-gallery');
-let wedding = './img/portfolioImages/wedding/';
-let child = './img/portfolioImages/child/';
-let special = './img/portfolioImages/special/';
-let headshot = './img/portfolioImages/headshot/';
+let wedding = '/wedding/';
+let child = '/child/';
+let special = '/special/';
+let headshot = '/headshot/';
 
 // SORT BY WEDDING IMAGES
 function galSort(amount, type) {
-   var galImages = createImageArray(amount,type);
+   var galThumbImages = createImageThumbArray(amount,type);
+   var galFullImages = createImageFullArray(amount,type);
    if(portfolioGallery.querySelector('div')){
       portfolioGallery.removeChild(portfolioGallery.querySelector('div'));
    }
-   portfolioGallery.appendChild(makeImagesDiv(galImages));
+   portfolioGallery.appendChild(makeImagesDiv(galThumbImages, galFullImages));
 }
 
 // CREAT A DIV FULL OF IMAGES FROM RELEVENT ARRAY
-function makeImagesDiv(array) {
+function makeImagesDiv(thumbArray, fullArray) {
    // Create the containing div
    var imageDiv = document.createElement('div');
    // create each image and place in div
-   for(var i = 1;i < array.length;i++){
+   for(var i = 1;i < thumbArray.length;i++){
+      var imageATag = document.createElement('a');
       var imageEl = document.createElement('img');
-      imageEl.src = array[i];
-      imageDiv.appendChild(imageEl);
+      imageATag.href = fullArray[i];
+      imageEl.src = thumbArray[i];
+      imageATag.appendChild(imageEl);
+      imageDiv.appendChild(imageATag);
+      // Give lightbox class names
+      imageATag.className = "big"; 
+      imageDiv.className = "gallery"; 
    }
    // Finally, return the div
    return imageDiv;
 }
 
 // CREATE AN ARRAY OF IMAGE LOCATIONS
-function createImageArray(amount,folderPath){
-   let Images = [];
+function createImageThumbArray(amount,folderPath){
+   let thumbImages = [];
    for(let i = 1; i < amount + 1; i++){
-      Images[i] = folderPath + i + '.jpeg'
+      thumbImages[i] = './img/thumb/portfolio' + folderPath + '('+ i + ')' + '.jpg'
    }
-   return Images;
+   return thumbImages;
+}
+
+function createImageFullArray(amount,folderPath){
+   let fullImages = [];
+   for(let i = 1; i < amount + 1; i++){
+      fullImages[i] = './img/full/portfolio' + folderPath + '('+ i + ')' + '.jpg'
+   }
+   return fullImages;
 }
 
