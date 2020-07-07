6d7564d0b2771acd8bfa122790fbcf42c8da9e8d
Zibbly <owenhedwards@gmail.com>
2020-07-07 12:44:56 +0100

    Changed main page images to static images

--- Javascript/script.js
+++ Javascript/script.js
@@ -7,26 +7,26 @@ portfolioCategorySet = (category) => {
    localStorage.setItem('storageName',category);
 }
 
-var mainPageImageContainers = document.querySelectorAll('.mainGallImg');
-var mainPageImageLinks = document.querySelectorAll('.test-popup-link');
-var mainPageImages = [];
+// var mainPageImageContainers = document.querySelectorAll('.mainGallImg');
+// var mainPageImageLinks = document.querySelectorAll('.test-popup-link');
+// var mainPageImages = [];
 
-fetchMainPageGallery = () =>{
-   fetch('https://aqueous-badlands-87446.herokuapp.com/getmainpageimages', {
-   method: 'get',
-   headers: {'Content-Type' : 'application/json'},
-   })
-   .then(response => response.json())
-   .then(data => {
-      data.resources.forEach(element=>{
-         mainPageImages.push(element.url)
-      })
-      for(let i=0;i<mainPageImages.length;i++){
-         mainPageImageContainers[i].src=mainPageImages[i]
-         mainPageImageLinks[i].href=mainPageImages[i]
-      }
-   })
-   .catch(err=>{
-      console.log(err)
-   })
-}
+// fetchMainPageGallery = () =>{ 
+//    fetch('https://aqueous-badlands-87446.herokuapp.com/getmainpageimages', {
+//    method: 'get',
+//    headers: {'Content-Type' : 'application/json'},
+//    })
+//    .then(response => response.json())
+//    .then(data => {
+//       data.resources.forEach(element=>{
+//          mainPageImages.push(element.url)
+//       })
+//       for(let i=0;i<mainPageImages.length;i++){
+//          mainPageImageContainers[i].src=mainPageImages[i]
+//          mainPageImageLinks[i].href=mainPageImages[i]
+//       }
+//    })
+//    .catch(err=>{
+//       console.log(err)
+//    })
+// }
