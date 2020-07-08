056b60fb85ae013ba4535e4f840935fee8313eeb
Zibbly <owenhedwards@gmail.com>
2020-07-08 21:48:03 +0100

    Smooths portfolio loading, uses cookies to track portfolio tab

--- Javascript/portfolio.js
+++ Javascript/portfolio.js
@@ -7,6 +7,20 @@ let headshot = '/headshot/';
 let galleryImages = [];
 let portfolioGallery = document.querySelector('.port-gallery');
 let submitValue = '';
+const defaultCat = "wedding";
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return defaultCat;
+}
+
+
 fetchGalleryImages=(categoryInput)=>{
    if(categoryInput){
       submitValue = categoryInput;
@@ -14,69 +28,75 @@ fetchGalleryImages=(categoryInput)=>{
       submitValue = localStorage.getItem("storageName"); 
    }
    let galleryImages = [];
-   while (portfolioGallery.firstChild) {
-      portfolioGallery.removeChild(portfolioGallery.firstChild);
-   }
-
-   fetch('https://aqueous-badlands-87446.herokuapp.com/getgalleryimages', {
-      method: 'post',
-      headers: {'Content-Type' : 'application/json'},
-      body: JSON.stringify({
-         "type":submitValue
-      })
-   })
-   .then(response => response.json())
-   .then(data => {
-      data.resources.forEach(element=>{
-         galleryImages.push(element.url)
-      })
-
-      for(let i=0;i<galleryImages.length;i++){
-         let imageATag = document.createElement('a');
-         let imageEl = document.createElement('img');
-         let imageBox = document.createElement('div');
-
-         imageATag.href = galleryImages[i];
-         imageEl.src = galleryImages[i];
 
 
-         imageATag.className = "test-popup-link"; 
-         imageBox.className = "galImageBox"
-         imageEl.className = "galImage";
-
-         imageBox.appendChild(imageATag)
-
-         imageATag.appendChild(imageEl);
-
-         portfolioGallery.appendChild(imageBox)
+   portfolioGallery.classList.remove("loadedImg");
+   portfolioGallery.classList.add("loadingImg");
+   setTimeout(function()
+   { 
+      while (portfolioGallery.firstChild) {
+         portfolioGallery.removeChild(portfolioGallery.firstChild);
       }
-   })
-   .then(data=>{
-      $('.test-popup-link').magnificPopup({
-         type: 'image',
-         // other options
-         gallery: {
-            // options for gallery
-            enabled: true
-         },
-         zoom: {
-            enabled: true, // By default it's false, so don't forget to enable it
-
-            duration: 300, // duration of the effect, in milliseconds
-            easing: 'ease-in-out', // CSS transition easing function
-
-            // The "opener" function should return the element from which popup will be zoomed in
-            // and to which popup will be scaled down
-            // By defailt it looks for an image tag:
-            opener: function(openerElement) {
-               // openerElement is the element on which popup was initialized, in this case its <a> tag
-               // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-               return openerElement.is('img') ? openerElement : openerElement.find('img');
-            }
+      portfolioGallery.classList.remove("loadingImg");
+      portfolioGallery.classList.add("loadedImg");
+      fetch('https://aqueous-badlands-87446.herokuapp.com/getgalleryimages', {
+         method: 'post',
+         headers: {'Content-Type' : 'application/json'},
+         body: JSON.stringify({
+            "type":submitValue
+         })
+      })
+      .then(response => response.json())
+      .then(data => {
+         data.resources.forEach(element=>{
+            galleryImages.push(element.url)
+         })
+   
+         for(let i=0;i<galleryImages.length;i++){ 
+            let imageATag = document.createElement('a');
+            let imageEl = document.createElement('img');
+            let imageBox = document.createElement('div');
+   
+            imageATag.href = galleryImages[i];
+            imageEl.src = galleryImages[i];
+   
+            imageATag.className = "test-popup-link"; 
+            imageBox.className = "galImageBox"
+            imageEl.className = "galImage";
+   
+            imageBox.appendChild(imageATag)
+            imageATag.appendChild(imageEl);
+            portfolioGallery.appendChild(imageBox)
+   
+            imageBox.classList.add("loadingImg");
+            setTimeout(function(){ imageBox.classList.add("loadedImg"); }, 300);
          }
-      });
-   })
-   .catch(err=>{
-      console.log(err)
-   })
+      })
+      .then(data=>{
+         $('.test-popup-link').magnificPopup({
+            type: 'image',
+            // other options
+            gallery: {
+               // options for gallery
+               enabled: true
+            },
+            zoom: {
+               enabled: true, // By default it's false, so don't forget to enable it
+               duration: 300, // duration of the effect, in milliseconds
+               easing: 'ease-in-out', // CSS transition easing function
+               // The "opener" function should return the element from which popup will be zoomed in
+               // and to which popup will be scaled down
+               // By defailt it looks for an image tag:
+               opener: function(openerElement) {
+                  // openerElement is the element on which popup was initialized, in this case its <a> tag
+                  // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                  return openerElement.is('img') ? openerElement : openerElement.find('img');
+               }
+            }
+         });
+      })
+      .catch(err=>{
+         console.log(err)
+      })
+   }, 300);
 }
--- Javascript/script.js
+++ Javascript/script.js
@@ -3,30 +3,10 @@ function scrollA() {
    document.documentElement.scrollTop = 800; // For Chrome, Firefox, IE and Opera
 } 
 
+
 portfolioCategorySet = (category) => {
-   localStorage.setItem('storageName',category);
+   //localStorage.setItem('storageName',category);
+   document.cookie = 'portfolioCategory='+category+ '; expires= ; path=/; SameSite=None; Secure';
 }
 
-// var mainPageImageContainers = document.querySelectorAll('.mainGallImg');
-// var mainPageImageLinks = document.querySelectorAll('.test-popup-link');
-// var mainPageImages = [];
 
-// fetchMainPageGallery = () =>{ 
-//    fetch('https://aqueous-badlands-87446.herokuapp.com/getmainpageimages', {
-//    method: 'get',
-//    headers: {'Content-Type' : 'application/json'},
-//    })
-//    .then(response => response.json())
-//    .then(data => {
-//       data.resources.forEach(element=>{
-//          mainPageImages.push(element.url)
-//       })
-//       for(let i=0;i<mainPageImages.length;i++){
-//          mainPageImageContainers[i].src=mainPageImages[i]
-//          mainPageImageLinks[i].href=mainPageImages[i]
-//       }
-//    })
-//    .catch(err=>{
-//       console.log(err)
-//    })
-// }
