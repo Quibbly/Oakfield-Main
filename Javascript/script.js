00f1031a2eb8db302891ac2d0019a79890d2008b
Zibbly <owenhedwards@gmail.com>
2020-07-11 02:53:19 +0100

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,23 @@
 function scrollDown() {
    document.body.scrollTop = 0; // For Safari
-   if(screen.width>=1920)
+
+   if(screen.width>=3840)
+   {
+      document.documentElement.scrollTop = 2100; // For Chrome, Firefox, IE and Opera
+   }
+   else if(screen.width>=2560)
+   {
+      document.documentElement.scrollTop = 1400; // For Chrome, Firefox, IE and Opera
+   }
+   else if(screen.width>=1920)
    {
       document.documentElement.scrollTop = 1020; // For Chrome, Firefox, IE and Opera
    }
+   else
+   {
+      document.documentElement.scrollTop = 850; // For Chrome, Firefox, IE and Opera
+   }
+
    
 } 
 
