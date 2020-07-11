8d11613ac53c141e0c272b64dae93bb90cbad203
Zibbly <owenhedwards@gmail.com>
2020-07-11 16:58:57 +0100

    Update pricing.js

--- Javascript/pricing.js
+++ Javascript/pricing.js
@@ -1,10 +1,13 @@
 function reveal(cat) {
    var catBox = document.querySelector("."+cat+"Content");
+
    if (!catBox.classList.contains(cat+"ContentRevealed")) {
-      catBox.classList.remove(cat+"ContentHidden")
-      catBox.classList.add(cat+"ContentRevealed")
-   } else {
-      catBox.classList.remove(cat+"ContentRevealed")
-      catBox.classList.add(cat+"ContentHidden")
+      catBox.classList.remove(cat+"ContentHidden");
+      catBox.classList.add(cat+"ContentRevealed");
+      ScrollReveal({ duration: 1600 }).reveal(catBox);
+   } 
+   else {
+      catBox.classList.remove(cat+"ContentRevealed");
+      catBox.classList.add(cat+"ContentHidden");
    }
 } 
